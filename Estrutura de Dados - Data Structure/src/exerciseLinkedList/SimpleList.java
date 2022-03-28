a88c54125f709f26d90d51b3826e3aebd7368ab7
Lucas Farias <82104519+lucasfariassa@users.noreply.github.com>
2022-03-28 09:19:31 -0300

    Last method added

--- Estrutura de Dados - Data Structure/src/exerciseLinkedList/SimpleList.java
+++ Estrutura de Dados - Data Structure/src/exerciseLinkedList/SimpleList.java
@@ -1,48 +1,48 @@
 package exerciseLinkedList;
 
 public class SimpleList {
-    Node head = null; // defini√ß√£o do in√≠cio da lista - definition of the head of the list
-    int size = 0; // vari√°vel de tamanho da lista - list size variable
+    Node head = null; // definiÁ„o do inÌcio da lista - definition of the head of the list
+    int size = 0; // vari·vel de tamanho da lista - list size variable
 
-    public void insertHead(String info) { // inserir no in√≠cio da lista - insert a node at the head of the list
+    public void insertHead(String info) { // inserir no inÌcio da lista - insert a node at the head of the list
         Node node = new Node();
-        node.info = info; // registra o dado recebido pelo par√¢metro ‚Äî registers the data received from parameter
-        node.next = head; /* o "pr√≥ximo" do novo n√≥ aponta para a atual localiza√ß√£o do in√≠cio da lista -
+        node.info = info; // registra o dado recebido pelo par‚metro - registers the data received from parameter
+        node.next = head; /* o "prÛximo" do novo nÛ aponta para a atual localizaÁ„o do inÌcio da lista -
 							the "next" of the new node point to current localization of the head of the list */
-        head = node; // o in√≠cio se torna o n√≥ rec√©m criado - the head become the newly created node
-        size++; /* aumenta a lista, pois foi adicionado um novo n√≥ - increases the list because a new node
+        head = node; // o in√≠cio se torna o nÛ rec√©m criado - the head become the newly created node
+        size++; /* aumenta a lista, pois foi adicionado um novo nÛ - increases the list because a new node
                 has just been created */
     }
 
     public void insertTail(String info) { // inserir no final da lista - insert a node at the tail of the list
         Node node = new Node();
         node.info = info;
-        if (head == null) { /* verifica se h√° apenas um n√≥ existente para adicionar um ao final dele -
+        if (head == null) { /* verifica se h· apenas um nÛ existente para adicionar um ao final dele -
 							checks if there is only one node in the list to be able to insert a new one
 							at the end of it */
             head = node;
             node.next = null;
-        } else { /* verifica quantos n√≥s existem para adicionar um ao final - checks how many nodes there are
+        } else { /* verifica quantos nÛs existem para adicionar um ao final - checks how many nodes there are
                     to add one to the end */
-            Node local = head; /* ponteiro auxiliar de localiza√ß√£o (inicialmente aponta para o in√≠cio da lista) -
+            Node local = head; /* ponteiro auxiliar de localizaÁ„o (inicialmente aponta para o inÌcio da lista) -
                                 location helper pointer (initially points to the beginning of the list) */
-            while (local.next != null) { /* verifica se o "pr√≥ximo" do n√≥ est√° apontando para null - checks if the
-                                        "next" of the node is pointing to null*/
-                local = local.next; /* caso o n√≥ aponte para o null, o ponteiro "local" finalmente encontra
-                                    o √∫ltimo n√≥ - if the node points to null, the "local" pointer finally finds
+            while (local.next != null) { /* verifica se o "prÛximo" do nÛ est· apontando para null - checks if the
+                                        "next" of the node is pointing to null */
+                local = local.next; /* caso o nÛ aponte para o null, o ponteiro "local" finalmente encontra
+                                    o ˙ltimo nÛ - if the node points to null, the "local" pointer finally finds
                                     the last node */
             }
-            local.next = node; /* define que o n√≥ que est√° na localiza√ß√£o atual do ponteiro "local" agora aponte para
-                                o n√≥ rec√©m-criado - sets that the node that is at the current location of the "local"
+            local.next = node; /* define que o nÛ que est· na localizaÁ„o atual do ponteiro "local" agora aponte para
+                                o nÛ recÈm-criado - sets that the node that is at the current location of the "local"
                                 pointer will now point to the newly created node */
-            node.next = null; /* define que o n√≥ rec√©m criado aponte para o null, fazendo-o se tornar o √∫ltimo da
+            node.next = null; /* define que o nÛ recÈm criado aponte para o null, fazendo-o se tornar o ˙ltimo da
                                  lista - sets the newly created node to point to null, making it the last one
                                  in the list */
             size++;
         }
     }
 
-    public String removeNode(int position) { /* remover um n√≥ de uma posi√ß√£o desejada ‚Äî remove a node from a specific
+    public String removeNode(int position) { /*  remover um nÛ de uma posiÁ„o desejada ó remove a node from a specific
                                                 desired position */
         if(position < 0 || position >= size || head == null) {
             return null;
@@ -52,42 +52,44 @@ public class SimpleList {
             return removeTail();
         }
         Node local = head;
-        for (int i = 0; i < position-1; i++) { /* localiza o n√≥ anterior ao que se deseja remover - finds the node
+        for (int i = 0; i < position-1; i++) { /* localiza o nÛ anterior ao que se deseja remover - finds the node
                                                 before the one you want to remove */
-            local = local.next; /* salva a posi√ß√£o do que o n√≥ exclu√≠do est√° apontando - saves the position of what
+            local = local.next; /* salva a posiÁ„o do que o nÛ excluÌdo est· apontando - saves the position of what
                                 the deleted node is pointing to */
         }
-        String info = local.next.info; // exibe a informa√ß√£o apagada - show the information erased
-        local.next = local.next.next; /* define que o "pr√≥ximo" do "pr√≥ximo" (o elemento ap√≥s ao que se deseja remover)
-                                        ser√° o novo "pr√≥ximo" do n√≥ anterior ao que foi retirado - defines that the
+        String info = local.next.info; // exibe a informaÁ„o apagada - show the information erased
+        local.next = local.next.next; /* define que o "prÛximo" do "prÛximo" (o elemento apÛs ao que se deseja remover)
+                                        ser· o novo "prÛximo" do nÛ anterior ao que foi retirado - defines that the
                                         "next" of the "next" (the element after the one to be removed) will be the
                                         new "next" of the node before the one that was removed */
         size--;
         return info;
     }
 
-    private String removeHead() {
-        if (head == null) {
+    private String removeHead() { // remover o primeiro nÛ da lista
+        if (head == null) { // testa se a lista est· vazia - tests if the list is empty
             return null;
         }
-        String info = head.info;
-        head = head.next;
+        String info = head.info; // salva a informaÁ„o para printar o que foi apagado
+        head = head.next; // define o ponteiro para o prÛximo nÛ, colocando o segundo em primeiro lugar
         size--;
         return info;
     }
 
-    private String removeTail() {
-        if (head == null) {
+    private String removeTail() { // remover o ˙ltimo nÛ da lista
+        if (head == null) { // testa se a lista est· vazia - tests if the list is empty
             return null;
         }
-        Node local = head;
-        while (local.next != null) {
-            Node aux = local;
+        Node local = head; // inicializa um ponteiro no inÌcio da lista
+        while (local.next != null) { // procura o ˙ltimo nÛ atravÈs do ponteiro creiado anteriormente
+            Node aux = local; /* inicializa um ponteiro que aponta para o local atual e auxiliar na captura da informaÁ„o
+            					do nÛ atual */
             local = local.next;
-            if (local.next == null) {
+            if (local.next == null) { /* verifica se o ponteiro "local" do "prÛximo" est· apontando para o null 
+            							(observando que o ˙ltimo nÛ sempre aponta para null) */
                 aux.next = null;
                 size--;
-                return local.info;
+                return local.info; // retorna a informaÁ„o do que foi excluÌdo
             }
         }
         head = null;
@@ -98,8 +100,20 @@ public class SimpleList {
     public String listSize() { // exibir o tamanho da lista - show list size
         return "Tamanho da Lista - List Size: " + size;
     }
+    
+    public boolean findNameList() {
+    	Node local = beginning;
+    	boolean found = false;
+    	for (int i = 0; i < size; i++) {
+    		if (local.info == info) {
+    			found = true;
+    		}
+    		local = local.next;
+    	}
+    	return found;
+    }
 
-    public String toString() { /* sobrescri√ß√£o para personalizar a classe toString do Java - override to customize
+    public String toString() { /* sobrescriÁ„o para personalizar a classe toString do Java - override to customize
                                 Java's toString class */
         String str = "(" + size + ") ";
         Node local = head;
--- Estrutura de Dados - Data Structure/src/exerciseLinkedList/Test.java
+++ Estrutura de Dados - Data Structure/src/exerciseLinkedList/Test.java
@@ -5,12 +5,12 @@ public class Test {
         SimpleList sl = new SimpleList();
         System.out.println(sl);
 
-        // Testando o mÃ©todo insertHead() - Testing the method insertHead():
+        // Testando o método insertHead() - Testing the method insertHead():
         sl.insertHead("Hamilton Sobrinho");
         System.out.println(sl);
         System.out.println();
 
-        //Testando o mÃ©todo insertTail() - Testing the method insertTail():
+        //Testando o método insertTail() - Testing the method insertTail():
         sl.insertTail("Lucas Farias");
         System.out.println(sl);
         sl.insertTail("Oseias Lopes");
@@ -23,13 +23,16 @@ public class Test {
         System.out.println(sl);
         System.out.println();
 
-        // Testando o mÃ©todo listSize() - Testing the method listSize():
+        // Testando o método listSize() - Testing the method listSize():
         System.out.println(sl.listSize());
         System.out.println();
 
-        // Testando o mÃ©todo removeNode() - Testing the method removeNode():
+        // Testando o método removeNode() - Testing the method removeNode():
         System.out.print("O nÃ³ removido foi - The removed node was: ");
         System.out.println(sl.removeNode(5));
         System.out.println(sl);
+        
+        // Testando o método findNameList() - Testing the method findNameList():
+        sl.findNameList("Oseias Lopes");
     }
 }
